687e6cf8180489a882cd5686b1b95930df594fec
TomBriar <tombriar08@gmail.com>
2022-07-07 11:15:17 -0400

    Added varible length messages, And automated the encrypters key creation

--- include/secp256k1_ecies.h
+++ include/secp256k1_ecies.h
@@ -14,20 +14,21 @@ extern "C" {
  *  Returns: 1 when pubkey is valid.
  *  Args:    ctx:           pointer to a context object
  *  Out:     crypt32:       pointer to 32 bytes of encrypted message data.
- *  In:      m32:           pointer to 32 bytes of message data 
- *           a_pr32:        pointer to 32 bytes of entropy (must be unpredictable). Alice's private key.
+ *           a_ell64:       printer to 64 bytes of an encoded eliptic curve point(See ellsq). Alice's ELLSQ encoded public elptic curve point.
+ *  In:      plain:           pointer to 32 bytes of message data 
+ *           rnd32:         pointer to 32 bytes of entropy (must be unpredictable). Alice's private key.
  *           b_ell64:       pointer to 64 bytes of an encoded eliptic curve point(See ellsq). Bob's ELLSQ encoded public eliptic curve point.
- *           rnd16:         pointer to 16 bytes of entropy (must be unpredictable). iv input for AES
  *
  * This function runs in unknown time.
  */
 SECP256K1_API int secp256k1_ecies_encrypt(
     const secp256k1_context* ctx,
-    unsigned char *crypt32,
-    const unsigned char *m32,
-    const unsigned char *a_pr32,
-    const unsigned char *b_ell64,
-    const unsigned char *rnd32
+    unsigned char *crypt,
+    unsigned char *a_ell64,
+    const unsigned char *plain,
+    const size_t size,
+    const unsigned char *rnd32,
+    const unsigned char *b_ell64
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4) SECP256K1_ARG_NONNULL(5);
 
 /* Decrypt a message with ECIES.
@@ -36,20 +37,19 @@ SECP256K1_API int secp256k1_ecies_encrypt(
  *  Args:    ctx:           pointer to a context object
  *  Out:     rem32:         pointer to 32 bytes of unencrypted message data.
  *  In:      crypt32:       pointer to 32 bytes of encrypted message data 
- *           b_pr32:        pointer to 32 bytes of entropy (must be unpredictable). Bob's private key.
+ *           rnd32:         pointer to 32 bytes of entropy (must be unpredictable). Bob's private key.
  *           a_ell64:       pointer to 64 bytes of an encoded eliptic curve point(See ellsq). Alice's ELLSQ encoded public eliptic curve point.
- *           iv16:          pointer to 16 bytes of entropy (must be unpredictable). iv input for AES
  *
  * This function runs in unknown time.
  */
 
 SECP256K1_API int secp256k1_ecies_decrypt(
     const secp256k1_context* ctx,
-    unsigned char *rem32,
+    unsigned char *plain,
     const unsigned char *crypt32,
-    const unsigned char *b_pr32,
-    const unsigned char *a_ell64,
-    const unsigned char *iv16
+    const size_t size,
+    const unsigned char *rnd32,
+    const unsigned char *a_ell64
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4) SECP256K1_ARG_NONNULL(5);
 
 
--- src/modules/ecies/main_impl.h
+++ src/modules/ecies/main_impl.h
@@ -12,38 +12,48 @@
 #include "../../../include/secp256k1_ecdh.h"
 #include "../../../include/secp256k1_ellsq.h"
 #include "../../../include/secp256k1_aes.h"
+#include "math.h"
 
 
 
-int secp256k1_ecies_encrypt(const secp256k1_context* ctx, unsigned char *crypt32, const unsigned char *m32, const unsigned char *a_pr32, const unsigned char *b_ell64, const unsigned char *rnd16) {
+int secp256k1_ecies_encrypt(const secp256k1_context* ctx, unsigned char *crypt, unsigned char *a_ell64, const unsigned char *plain, const size_t size, const unsigned char *rnd32, const unsigned char *b_ell64) {
 	secp256k1_scalar a_pr;
 	secp256k1_pubkey b_p, a_p;
     secp256k1_ge b_g, a_g;
     secp256k1_gej a_gej;
     AES256_CBC_ctx aes_ctx;
+    secp256k1_sha256 hash;
     
-    unsigned char symetric_key[32];
+    unsigned char symetric_key[32], rnd32_2[32], hashIter;
     int overflow;
     
     VERIFY_CHECK(ctx != NULL);
-    ARG_CHECK(m32 != NULL);
-    ARG_CHECK(a_pr32 != NULL);
+    ARG_CHECK(plain != NULL);
+    ARG_CHECK(rnd32 != NULL);
     ARG_CHECK(b_ell64 != NULL);
 
     secp256k1_ellsq_decode(ctx, &b_p, b_ell64);
 
     if (secp256k1_pubkey_load(ctx, &b_g, &b_p)) {
 
-    	secp256k1_scalar_set_b32(&a_pr, a_pr32, &overflow);
-    	secp256k1_ecmult_gen(&ctx->ecmult_gen_ctx, &a_gej, &a_pr);
-    	secp256k1_ge_set_gej(&a_g, &a_gej);
-    	secp256k1_pubkey_save(&a_p, &a_g);
+	    secp256k1_scalar_set_b32(&a_pr, rnd32, &overflow);
+	    secp256k1_ecmult_gen(&ctx->ecmult_gen_ctx, &a_gej, &a_pr);
+	    secp256k1_ge_set_gej(&a_g, &a_gej);
+	    secp256k1_pubkey_save(&a_p, &a_g);
 
-    	CHECK(secp256k1_ecdh(ctx, symetric_key, &b_p, a_pr32, NULL, NULL));
+        secp256k1_sha256_initialize(&hash);
+        secp256k1_sha256_write(&hash, rnd32, sizeof(rnd32));
+        hashIter = '1';
+        secp256k1_sha256_write(&hash, &hashIter, sizeof("1"));
+        secp256k1_sha256_finalize(&hash, rnd32_2);
 
+	    secp256k1_ellsq_encode(ctx, a_ell64, rnd32_2, &a_p);
 
-    	AES256_CBC_init(&aes_ctx, symetric_key, rnd16);
-    	AES256_CBC_encrypt(&aes_ctx, 2, crypt32, m32);
+    	CHECK(secp256k1_ecdh(ctx, symetric_key, &b_p, rnd32, NULL, NULL));
+    	
+
+    	AES256_CBC_init(&aes_ctx, symetric_key, a_ell64);
+    	AES256_CBC_encrypt(&aes_ctx, ceil(size / 16), crypt, plain);
 
         return 1;
     }
@@ -53,8 +63,8 @@ int secp256k1_ecies_encrypt(const secp256k1_context* ctx, unsigned char *crypt32
 
 
 
-int secp256k1_ecies_decrypt(const secp256k1_context* ctx, unsigned char *rem32, const unsigned char *crypt32, const unsigned char *b_pr32, const unsigned char *a_ell64, const unsigned char *iv16) {
-    secp256k1_scalar crypt, b_pr;
+int secp256k1_ecies_decrypt(const secp256k1_context* ctx, unsigned char *plain, const unsigned char *crypt, const size_t size, const unsigned char *rnd32, const unsigned char *a_ell64) {
+    secp256k1_scalar b_pr;
 	secp256k1_pubkey b_p, a_p;
     secp256k1_ge b_g, a_g;
     secp256k1_gej b_gej;
@@ -64,24 +74,24 @@ int secp256k1_ecies_decrypt(const secp256k1_context* ctx, unsigned char *rem32, 
     int overflow;
     
     VERIFY_CHECK(ctx != NULL);
-    ARG_CHECK(crypt32 != NULL);
-    ARG_CHECK(b_pr32 != NULL);
+    ARG_CHECK(crypt != NULL);
+    ARG_CHECK(rnd32 != NULL);
     ARG_CHECK(a_ell64 != NULL);
 
     secp256k1_ellsq_decode(ctx, &a_p, a_ell64);
 
     if (secp256k1_pubkey_load(ctx, &a_g, &a_p)) {
-    	secp256k1_scalar_set_b32(&b_pr, b_pr32, &overflow);
-    	secp256k1_scalar_set_b32(&crypt, crypt32, &overflow);
+    	secp256k1_scalar_set_b32(&b_pr, rnd32, &overflow);
     	
     	secp256k1_ecmult_gen(&ctx->ecmult_gen_ctx, &b_gej, &b_pr);
     	secp256k1_ge_set_gej(&b_g, &b_gej);
     	secp256k1_pubkey_save(&b_p, &b_g);
 
-    	CHECK(secp256k1_ecdh(ctx, symetric_key, &a_p, b_pr32, NULL, NULL));
+    	CHECK(secp256k1_ecdh(ctx, symetric_key, &a_p, rnd32, NULL, NULL));
 
-    	AES256_CBC_init(&aes_ctx, symetric_key, iv16);
-    	AES256_CBC_decrypt(&aes_ctx, 2, rem32, crypt32);
+    	AES256_CBC_init(&aes_ctx, symetric_key, a_ell64);
+    	AES256_CBC_decrypt(&aes_ctx, ceil(size / 16), plain, crypt);
+
         return 1;
     }
     /* Only returned in case the provided pubkey is invalid. */
